1f559f5178f4d8271b0bdd6623e7b7676ddf6ea1
Андрей Мараков <Tyuiu.MarakovAD@outlook.com>
2025-09-15 11:29:49 +0500

    Task2

---
+++ Tyuiu.MarakovAD.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.MarakovAD.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.MarakovAD.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Андрей";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Андрей", res);
+        }
+    }
+}
---
+++ Tyuiu.MarakovAD.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MarakovAD.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.MarakovAD.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Андрей"));
+            Console.ReadKey();
+        }
+    }
+}
