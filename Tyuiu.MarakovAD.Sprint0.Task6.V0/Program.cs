313855da67b9725bf47ff3dc7cee194e232bc84a
Андрей Мараков <Tyuiu.MarakovAD@outlook.com>
2025-09-15 13:16:46 +0500

    Task6

---
+++ Tyuiu.MarakovAD.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MarakovAD.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.MarakovAD.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubstractionArray(numsArray));
+            Console.WriteLine("Умножение элементов массива = " + DataService.MultArray(numsArray));
+            Console.ReadKey();
+        }
+    }
+}
