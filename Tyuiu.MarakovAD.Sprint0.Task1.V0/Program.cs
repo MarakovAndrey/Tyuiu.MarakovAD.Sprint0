c94e714b7ad039eafacd57b6f2909b0cc5327078
Андрей Мараков <Tyuiu.MarakovAD@outlook.com>
2025-09-15 10:30:59 +0500

    Task1

---
+++ Tyuiu.MarakovAD.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Hello, World!");
+        string name = "Андрей";
+        int age = 18;
+        Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет!");
+        Console.ReadKey();
+    }
+}
