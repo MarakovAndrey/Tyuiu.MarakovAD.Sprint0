a3a866167947cbb167f33f0e358218cfa9c84c20
Андрей Мараков <Tyuiu.MarakovAD@outlook.com>
2025-09-15 12:36:59 +0500

    Task4

---
+++ Tyuiu.MarakovAD.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.MarakovAD.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.MarakovAD.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        public void CheckedSubstractionValid() {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(2, DataService.Division(10, 5));
+        }
+    }
+}
