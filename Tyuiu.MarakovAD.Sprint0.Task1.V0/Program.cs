4a6d574788b34e30c5dfe7e85601a576129c34b8
Андрей Мараков <Tyuiu.MarakovAD@outlook.com>
2025-09-15 10:42:46 +0500

    Task1Dop

--- Tyuiu.MarakovAD.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.MarakovAD.Sprint0.Task1.V0/Program.cs
@@ -1,9 +1,18 @@
-﻿class Program {
-    static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
-        string name = "Андрей";
-        int age = 18;
-        Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет!");
-        Console.ReadKey();
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Tyuiu.MarakovAD.Sprint0.Task1.V0 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Андрей";
+            int age = 18;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
     }
 }
